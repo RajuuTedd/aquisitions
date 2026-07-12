56acad365bc060a550f444556ac3b8ee1c0423b4
RajuuTedd <siddharthbabel082@gmail.com>
2026-07-12 20:33:04 +0530

    added lint

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app =express();
 
 app.get('/', (req,res)=> {
-    res.status(200).send('Hello from Aquisitions!')
-})
+  res.status(200).send('Hello from Aquisitions!');
+});
 
 export default app;
